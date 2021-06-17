73e9e807ae1dea18e812ead888441b8407e888a2
zhangqc <zhang13402930485@163.com>
2021-06-18 00:09:34 +0800

    添加utils包

---
+++ atcrowdfunding03-admin-component/src/main/java/utils/UtilsTest.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class UtilsTest {
+    public static void main(String[] args) {
+        System.out.println("你写这么长的项目名干几把毛，幺哥");
+        System.out.println("你写这么长的项目名干几把毛，幺哥");
+        System.out.println("你写这么长的项目名干几把毛，幺哥");
+        System.out.println("你写这么长的项目名干几把毛，幺哥");
+        System.out.println("你写这么长的项目名干几把毛，幺哥");
+        System.out.println("你写这么长的项目名干几把毛，幺哥");
+    }
+}
