4b9f3b3198cc6241c3044310490b2284ee1b31e5
zhangqc <zhangqc@asiainfo.com>
2021-06-18 10:12:59 +0800

    add module file

---
+++ atcrowdfunding03-admin-component/src/main/java/utils/AppTest.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * User: ZhangQianChao
+ * Date: 2021/6/18
+ * Time: 10:03
+ * Description:
+ */
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println(345);
+    }
+}
