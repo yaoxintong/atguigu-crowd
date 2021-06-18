dfc99dcee3255e8ccd51e91074e5af9789ec9b06
zhangqc <zhangqc@asiainfo.com>
2021-06-18 13:25:03 +0800

    web config

---
+++ atcrowdfunding02-admin-webui/src/main/java/top/duanhong/act/controller/ControllerDemo.java
@@ -0,0 +1,16 @@
+package top.duanhong.act.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * User: ZhangQianChao
+ * Date: 2021/6/18
+ * Time: 12:45
+ * Description:
+ */
+@Controller
+@RequestMapping("/test/demo")
+public class ControllerDemo {
+
+}
